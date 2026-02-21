721e953575ed2ec8e13a2cdbfa10541d77b2af90
zinzo <zinzoddari@gmail.com>
2026-02-21 12:03:12 +0900

    init commit

---
+++ src/main/java/com/onlogn/onlogn/OnlognApplication.java
@@ -0,0 +1,13 @@
+package com.onlogn.onlogn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlognApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlognApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlogn/onlogn/OnlognApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlogn.onlogn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlognApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
